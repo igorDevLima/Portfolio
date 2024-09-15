0b15321c44fbbe89db86317f6042095e4fd306df
IgorLima <igorlimadrum@gmail.com>
2024-09-15 14:46:52 -0300

    style: add gradient effect in button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,25 @@
+import "./index.css";
 import MuiButton from "@mui/material/Button";
 import { ButtonProps } from "./types";
 import ButtonText from "../ButtonText";
 
-function Button({ children, ...props }: ButtonProps) {
+function Button({ children, sx, ...props }: ButtonProps) {
   return (
-    <MuiButton {...props}>
-      <ButtonText>{children}</ButtonText>
+    <MuiButton
+      className="button"
+      sx={{
+        background: "var(--color-text-primary)",
+        borderTopRightRadius: "0px",
+        borderTopLeftRadius: "0px",
+        borderBottomRightRadius: "0px",
+        borderBottomLeftRadius: "0px",
+        ...sx,
+      }}
+      {...props}
+    >
+      <ButtonText sx={{ color: "var(--color-background-default)" }}>
+        {children}
+      </ButtonText>
     </MuiButton>
   );
 }
